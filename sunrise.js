487afe554611da4a3cdeb3dac840c1a9cf78ee40
taegyunEH <taegyun98@naver.com>
2022-10-25 00:26:48 +0900

    Master

--- sunrise.js
+++ sunrise.js
@@ -14,27 +14,26 @@
     let cav = $('<canvas/>')
     let sunrise     = data.sys.sunrise;
     let sunset      = data.sys.sunset;
-    let gap         = (sunset - sunrise); 
+   
    
     let dt      = data.dt
     
 
-    let srx = (sunrise - td)/60;
-    let ssx = (sunset - td)/60;
-    let now =( dt -td)/60
+    let srx = Math.round((sunrise - td)/60);
+    let ssx = Math.round((sunset - td)/60);
+    let now = (dt -td)/60
+    let gap = ssx-srx; 
     let risegap = sunrise - td;
-    let sunhigh     = (srx + ssx)/2;
+    let sunhigh     = Math.round((srx + ssx)/2);
 
     let st =  $('.sun').innerWidth()/1440
 
     let t = st.toFixed(4);
 
-    console.log(t)
+    console.log(data)
 
 
     console.log(now*t)
-
-
 
 
 
@@ -64,14 +63,11 @@
        cav.attr('height', '80')  
    
     // ctx.bezierCurveTo(20, 100, 200, 100, 200, 20);
-    ctx.beginPath() 
-    ctx.arc( srx*t , 40, 6, 0, 2*Math.PI )
-    ctx.fillStyle = color;
-    ctx.fill();
 
-    ctx.beginPath() 
-    ctx.arc( ssx*t , 40, 6, 0, 2*Math.PI )
 
+    ctx.beginPath();
+    ctx.arc(srx*st, 40, 4 , 0, 2*Math.PI)
+    ctx.arc(ssx*st, 40, 4 , 0, 2*Math.PI)
     ctx.fillStyle = color;
     ctx.fill();
 
@@ -90,44 +86,26 @@
 
     ctx.beginPath();
     for(let i=0; i < 1440; i++){
-       let h = 40 + 20 * Math.sin( i * 2 * Pi/1440 + Pi/1440 * sunhigh)
-       ctx.lineTo( st*i, h)
-       ctx.lineWidth = 2;
-    //    ctx.arc(st*i,h,0.25,0,2*Math.PI)
+    //    let h =  20 * Math.sin(((sunhigh)/360 *  Pi * (i)+Pi/4)/1440) + 40
+       let h =  (i**4 + i**3 +i**2 +i)/1400
+        ctx.lineTo( st*i, h)
+       ctx.lineWidth = 1;
     }
+
     ctx.stroke();
 
-
-    console.log(srx)
-
-
-
-
-
-
-//      ctx.moveTo(-100,60)
-//      ctx.bezierCurveTo(-100,60,0,Math.sin(Pi/2)*30+60, srx*t,Math.sin(Pi/2)*30)
-//      ctx.stroke();
-
-
-//      ctx.beginPath() 
-//      ctx.moveTo(ssx*t,30)
-//      ctx.bezierCurveTo(ssx*t,30,333,Math.sin(Pi/2)*30+60,400,60)
-//      ctx.stroke();
     
+    ctx.beginPath();
 
-//      ctx.beginPath() 
-//      ctx.moveTo(srx*t,30)
-//      ctx.bezierCurveTo(srx*t,30,sunhigh*t ,(ssx-srx/2)*t,ssx*t,30)
-//      ctx.stroke();
+    let h =  20 * Math.sin(((ssx + srx)/720 * Pi * (now-sunhigh)+Pi/4)/1440) + 40
 
-//      ctx.beginPath() 
-//      ctx.arc( now*t , Math.sin(Pi/(2*(sunhigh))*now)*30, 6, 0, 2*Math.PI )
-//      ctx.fillStyle = color;
-//      ctx.fill();
+    ctx.arc( now*st , h , 6, 0, 2*Math.PI )
 
-    console.log((ssx-srx/2)*t)
+    ctx.fillStyle = color;
+    ctx.fill();
 
+    console.log(sunhigh)
+    console.log( srx)
 
 
 
